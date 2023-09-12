42d875fbf68a5109cd9977d1d426d8f56a588dcc
abhisek1221 <cherrymx317@gmail.com>
2023-09-12 23:59:12 +0530

    Database and admin panel

--- SlogHub/base/views.py
+++ SlogHub/base/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id':1, 'name':'Lets learn python'},
-    {'id':2, 'name':'UML'},
-    {'id':3, 'name':'OOPS'},
-]
+# rooms = [
+#     {'id':1, 'name':'Lets learn python'},
+#     {'id':2, 'name':'UML'},
+#     {'id':3, 'name':'OOPS'},
+# ]
+
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-        context = {'room': room}
+    room = Room.objects.get(id=pk)
+    context = {'room': room}
     return render(request, 'base/room.html', context)
