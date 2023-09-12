97486dcfff14cabd4c9f884f974a8bb44d27ad4f
abhisek1221 <cherrymx317@gmail.com>
2023-09-12 18:17:47 +0530

    sample addition of data to views

--- SlogHub/base/views.py
+++ SlogHub/base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+rooms = [
+    {'id':1, 'name':'Lets learn python'},
+    {'id':2, 'name':'UML'},
+    {'id':3, 'name':'OOPS'},
+]
+def home(request):
+    return render(request, 'home.html', {'rooms':rooms})
+
+def room(request):
+    return render(request, 'room.html')
