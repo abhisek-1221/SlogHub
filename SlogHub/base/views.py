e7722b96755418edc78374728890d60467cfa4b9
abhisek1221 <cherrymx317@gmail.com>
2023-09-22 12:17:17 +0530

    User registration

--- SlogHub/base/views.py
+++ SlogHub/base/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from .models import Room,Topic
@@ -14,7 +15,7 @@ def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -38,8 +39,22 @@ def logoutUser(request):
 
 def registerPage(request):
     page = 'register'
-    context = {'page': page}
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.usernmae.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error ocurred during registration')
+    context = {'form': form}
     return render(request, 'base/login_register.html', context)
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q')!= None else ''
     rooms = Room.objects.filter(
